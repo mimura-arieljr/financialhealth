fc484bea6ed16372bd8302f603fbb1b9480370ee
Ariel Jr Mimura <avmimurajr@gmail.com>
2026-03-04 08:56:55 +0800

    Add config to supabase for persisting sessions

--- client/src/lib/supabase.js
+++ client/src/lib/supabase.js
@@ -2,4 +2,12 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    storageKey: 'watcher-auth',
+    storage: window.localStorage,
+    autoRefreshToken: true,
+    detectSessionInUrl: false,
+  }
+})
